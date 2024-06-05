e7cae8ba941889c0fc05ac2126126683720be837
SemyonZh <semyonzhurkin@gmail.com>
2024-06-05 21:27:52 +0700

    feat: add era and gender option list

--- src/entities/character/model/character.ts
+++ src/entities/character/model/character.ts
@@ -1,6 +1,7 @@
 import {Movie} from "../../movie";
 import {Planet} from "../../planet";
 import {Species} from "../../species";
+import {SelectOptionType} from "../../../shared";
 
 export type Character = {
     id: number,
@@ -27,7 +28,17 @@ export enum BirthDayEra {
     ABY
 }
 
+export const eraOptions: SelectOptionType[] = [
+    {value: BirthDayEra.BBY.toString(), label: "ПБЯ"},
+    {value: BirthDayEra.ABY.toString(), label: "АБЯ"},
+];
+
 export enum CharacterGender {
     male,
     female
 }
+
+export const genderOptions: SelectOptionType[] = [
+    {value: CharacterGender.male.toString(), label: "Мужской"},
+    {value: CharacterGender.female.toString(), label: "Женский"},
+];
