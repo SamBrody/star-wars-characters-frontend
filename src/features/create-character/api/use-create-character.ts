fd8cee2b97737984002dcc49269166bc5d56fd9f
semyonzhurkin <semyonzhurkin@gmail.com>
2024-06-05 16:44:16 +0700

    refact: import fix

--- src/features/create-character/api/use-create-character.ts
+++ src/features/create-character/api/use-create-character.ts
@@ -1,6 +1,5 @@
 import {useMutation, useQueryClient} from "@tanstack/react-query";
-import {CreateCharacter} from "../../../entities/character/rdo/create-character.ts";
-import {createCharacter, CreateCharacterErrorKeys} from "../../../entities/character";
+import {CreateCharacter, createCharacter, CreateCharacterErrorKeys} from "../../../entities/character";
 
 export const useCreateCharacter = (onSuccess: () => void, onError: (error: Record<CreateCharacterErrorKeys, string[]>) => void) => {
     const queryClient = useQueryClient();
